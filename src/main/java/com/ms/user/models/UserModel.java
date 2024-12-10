429ad1b3a14347da9e3a8204ea39116f44825cf2
MarcosAdaSilva <marcaofly@gmail.com>
2024-12-10 14:26:50 -0300

    Criando microsservice producer com integração com RabbitMQ

---
+++ src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,20 @@
+package com.ms.user.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+    private static final long SerialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
